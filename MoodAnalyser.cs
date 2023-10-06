44480d005f05e466c42f6c23e99b5bbae449dc06
Vishal github <Vishal.kunchal@gmail.com>
2023-10-06 14:26:33 +0530

    [vishal kunchal]:Created Mood Analyser

---
+++ MoodAnalyser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception
+{
+    public class MoodAnalyser
+    {
+        private string message;
+
+        public MoodAnalyser(string message)
+        {
+            this.message = message;
+        }
+
+        public string analyse_mood()
+        {
+            if (this.message.Contains("Sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "Happy";
+            }
+        }
+    }
+}
